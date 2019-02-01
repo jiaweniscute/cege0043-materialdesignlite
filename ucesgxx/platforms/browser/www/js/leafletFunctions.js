2c09ba9b8b47a9e4fbb87ad3b18e4518b606f561
jiaweniscute <ng.jia.wen@hotmail.com>
2019-02-01 05:01:14 -0500

    added earthquakes layer and point layer functions

--- ucesgxx/platforms/browser/www/js/leafletFunctions.js
+++ ucesgxx/platforms/browser/www/js/leafletFunctions.js
@@ -1,20 +1,27 @@
 
 
+
+var client;
+var earthquakelayer;
+var mypoint;
+var mycircle;
+var mypolygon;
+
 function addPointLinePoly(){
 
     // add a point
-    L.marker([51.5,-0.09]).addTo(mymap)
+    mypoint = L.marker([51.5,-0.09]).addTo(mymap)
         .bindPopup("<b>Hello!</b><br/>I am a popup.").openPopup();
 
     // add a circle
-    L.circle([51.5, -0.11], 500, {
+    mycircle = L.circle([51.5, -0.11], 500, {
         color: 'red',
         fillColor: '#f03',
         fillOpacity:0.5
     }).addTo(mymap).bindPopup("I am a circle")
 
     // add a polygon (triangle)
-    var myPolygon = L.polygon([
+    mypolygon = L.polygon([
         [51.509, -0.08],
         [51.503, -0.06],
         [51.51, -0.047]
@@ -27,4 +34,38 @@ function addPointLinePoly(){
 
 
 }
+
+
+function getEarthquakes(){
+    client = new XMLHttpRequest();
+    var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+    client.open('GET', url);
+    client.onreadystatechange = dataResponse;
+    client.send();
+
+}
+
+function removeLayers(){
+    mymap.removeLayer(earthquakelayer);
+    mymap.removeLayer(mypoint)
+    mymap.removeLayer(mycircle)
+    mymap.removeLayer(mypolygon)
+
+
+}
+
+function dataResponse() {
+    if (client.readyState == 4) {
+        var geoJSONData = client.responseText;
+        console.log('loading data')
+        loadLayer(geoJSONData);
+    }
+}
+
+function loadLayer(geoJSONData){
+    var json = JSON.parse(geoJSONData);
+    earthquakelayer = L.geoJson(json).addTo(mymap);
+    mymap.fitBounds(earthquakelayer.getBounds());
+}
+
 
